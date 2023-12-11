000cffff1cf4fcbc0b837536f79dd0eb6a256283
murad <kh4lidov@mail.ru>
2023-12-11 05:03:10 +0300

    add a new prop

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,7 +1,8 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/react";
+import { MAX_WIDTH_CLASS } from "@/Components/Common/MaxWidthClass.js";
 
-export default function Guest({ children }) {
+export default function Guest({ children, maxWidth }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
@@ -10,7 +11,11 @@ export default function Guest({ children }) {
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div
+                className={`w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg ${
+                    maxWidth ? MAX_WIDTH_CLASS[maxWidth] : "max-w-md"
+                }`}
+            >
                 {children}
             </div>
         </div>
