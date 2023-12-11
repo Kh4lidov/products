92a8ddb54728fd1a3c8bfebd51b23dcfa6b23475
murad <kh4lidov@mail.ru>
2023-12-11 04:10:50 +0300

    add a new prop

--- resources/js/Components/Products/ProductForm.jsx
+++ resources/js/Components/Products/ProductForm.jsx
@@ -3,8 +3,12 @@ import TextInput from "@/Components/TextInput.jsx";
 import InputError from "@/Components/InputError.jsx";
 import SelectBox from "@/Components/SelectBox.jsx";
 import ProductProps from "@/Components/Products/ProductProps.jsx";
+import { usePage } from "@inertiajs/react";
+import { PRODUCT_FORM_MODE } from "@/Components/Common/Products/ProductFormMode.js";
 
-export default function ProductForm({ form, statusTypes }) {
+export default function ProductForm({ form, statusTypes, mode }) {
+    const { auth } = usePage().props;
+
     const handleProductDataChange = (d) => {
         form.setData("data", d);
     };
@@ -20,9 +24,12 @@ export default function ProductForm({ form, statusTypes }) {
                 <TextInput
                     id="article"
                     name="article"
-                    className="mt-2 w-[30rem]"
+                    className="mt-2 w-[30rem] disabled:bg-gray-100/50 disabled:border-gray-400"
                     value={form.data.article || ""}
                     onChange={(e) => form.setData("article", e.target.value)}
+                    disabled={
+                        mode === PRODUCT_FORM_MODE.Edit && !auth.user.is_admin
+                    }
                 />
                 <InputError className="mt-2" message={form.errors.article} />
             </div>
